2c3721205319f0549405d5458221587874480427
Imre Nagi <imre.nagi@gojek.com>
2022-12-17 15:14:06 -0500

    separating drawer
    Signed-off-by: Imre Nagi <imre.nagi@gojek.com>

--- display.go
+++ display.go
@@ -19,32 +19,20 @@ func DefaultDisplay() *Display {
 	d := &Display{
 		H:      32,
 		W:      64,
-		screen: tcellScreen,
+		drawer: NewTcellDisplay(tcellScreen),
 	}
 	d.init()
 	return d
 }
 
 type Display struct {
-	displayer Displayer
-	screen    tcell.Screen
+	drawer Drawer
 
 	H, W uint8
 	data [][]uint8
 }
 
 func (d *Display) init() {
-	encoding.Register()
-	if err := d.screen.Init(); err != nil {
-		log.Fatal().Msgf("not able to init screen")
-	}
-
-	defStyle := tcell.StyleDefault.
-		Background(tcell.ColorReset).
-		Foreground(tcell.ColorReset)
-	d.screen.SetStyle(defStyle)
-	d.screen.Clear()
-
 	row := make([][]uint8, d.H)
 	for r := 0; r < int(d.H); r++ {
 		colums := make([]uint8, d.W)
@@ -55,25 +43,10 @@ func (d *Display) init() {
 	}
 	d.data = row
 
-	go func() {
-		for {
-			switch ev := d.screen.PollEvent().(type) {
-			case *tcell.EventResize:
-				d.screen.Sync()
-			case *tcell.EventKey:
-				if ev.Key() == tcell.KeyEscape {
-					d.screen.Fini()
-					os.Exit(0)
-				} else if ev.Key() == tcell.KeyRune {
-					fmt.Println("pressed", ev.Rune())
-				}
-			}
-		}
-	}()
 }
 
 func (d *Display) Clear() {
-	d.screen.Clear()
+	d.drawer.Clear()
 }
 
 func (d *Display) SetPixel(x, y uint8, val uint8) {
@@ -85,48 +58,77 @@ func (d *Display) GetPixel(x, y uint8) uint8 {
 }
 
 func (d *Display) Draw() {
-	d.screen.Clear()
-	style := tcell.StyleDefault.Background(tcell.ColorDarkRed)
+	d.drawer.Clear()
 	for y, r := range d.data {
 		for x, c := range r {
 			if c > 0 {
-				emitStr(d.screen, x, y, style, " ")
+				d.drawer.SetPixel(x, y)
 			}
 		}
 	}
-	d.screen.Show()
+	d.drawer.Draw()
 }
 
-func emitStr(s tcell.Screen, x, y int, style tcell.Style, str string) {
-	for _, c := range str {
-		var comb []rune
-		w := runewidth.RuneWidth(c)
-		if w == 0 {
-			comb = []rune{c}
-			c = ' '
-			w = 1
-		}
-		s.SetContent(x, y, c, comb, style)
-		x += w
-	}
-}
-
-type Displayer interface {
+type Drawer interface {
 	Clear()
+	SetPixel(x, y int)
 	Draw()
 }
 
+func NewTcellDisplay(screen tcell.Screen) *TcellDisplay {
+	encoding.Register()
+	if err := screen.Init(); err != nil {
+		log.Fatal().Msgf("not able to init screen")
+	}
+
+	defStyle := tcell.StyleDefault.
+		Background(tcell.ColorReset).
+		Foreground(tcell.ColorRed)
+	screen.SetStyle(defStyle)
+	screen.Clear()
+
+	go func() {
+		for {
+			switch ev := screen.PollEvent().(type) {
+			case *tcell.EventResize:
+				screen.Sync()
+			case *tcell.EventKey:
+				if ev.Key() == tcell.KeyEscape {
+					screen.Fini()
+					os.Exit(0)
+				} else if ev.Key() == tcell.KeyRune {
+					fmt.Println("pressed", ev.Rune())
+				}
+			}
+		}
+	}()
+
+	return &TcellDisplay{
+		screen: screen,
+	}
+}
+
 type TcellDisplay struct {
+	screen tcell.Screen
 }
 
-func (t TcellDisplay) Draw() {
-
+func (t *TcellDisplay) SetPixel(x, y int) {
+	c := ' '
+	var comb []rune
+	w := runewidth.RuneWidth(c)
+	if w == 0 {
+		comb = []rune{c}
+		c = ' '
+		w = 1
+	}
+	t.screen.SetContent(x, y, c, comb,
+		tcell.StyleDefault.Background(tcell.ColorRed))
 }
 
-func (t TcellDisplay) Clear() {
-
+func (t *TcellDisplay) Clear() {
+	t.screen.Clear()
 }
 
-func (t TcellDisplay) Show() {
-
+func (t *TcellDisplay) Draw() {
+	t.screen.Show()
 }
